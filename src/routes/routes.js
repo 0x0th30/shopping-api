2c36c4c59fed18a63b25af697519b445debbc7d6
Theo Silva <theodorotheodore@gmail.com>
2021-10-07 10:26:56 -0300

    Unstaged changes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const controllers = require('../controllers');
+const controller = require('../controllers');
 
 const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-router.post('/items/create', controllers.storeItem);
-router.get('/items/cart', controllers.viewCart);
+router.post('/items/create', controller.storeItem);
+router.get('/items/cart', controller.viewCart);
 
 module.exports = router;
