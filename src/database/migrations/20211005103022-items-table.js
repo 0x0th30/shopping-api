449d1088d95296f4dc46765994fc8ebe83e7b11a
Theo Silva <theodorotheodore@gmail.com>
2021-10-25 11:45:56 -0300

    Added docker files

--- src/database/migrations/20211005103022-items-table.js
+++ src/database/migrations/20211005103022-items-table.js
@@ -17,6 +17,14 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
     });
   },
 
@@ -24,3 +32,4 @@ module.exports = {
     return queryInterface.dropTable('items');
   }
 };
+ 
